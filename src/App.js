544ef482fa309429fb453dd8c3a39efb2c2011e6
komporbakar <65328980+komporbakar@users.noreply.github.com>
2023-03-21 04:42:17 +0700

    learn-movie-list

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { getMovieList, searchMovie } from "./Api";
+import { useEffect, useState } from "react";
 
-function App() {
+const App = () => {
+  const [popularMovies, setPopularMovies] = useState([]);
+  useEffect(() => {
+    getMovieList().then((result) => {
+      setPopularMovies(result);
+    });
+  }, []);
+
+  const PopularMovieList = () => {
+    return popularMovies.map((movie, i) => {
+      if (!movie) {
+        return <h1>Not Found</h1>;
+      } else {
+        return (
+          <div className="Movie-wrapper" key={i}>
+            <div className="Movie-title">{movie.title}</div>
+            <img
+              src={`${process.env.REACT_APP_BASEIMAGEURL}/${movie.poster_path}`}
+              className="Movie-image"
+            />
+            <div className="Movie-date">{movie.release_date}</div>
+            <div className="Movie-rate">{movie.vote_average}</div>
+          </div>
+        );
+      }
+    });
+  };
+
+  const search = async (q) => {
+    if (q.length > 3) {
+      const query = await searchMovie(q);
+      setPopularMovies(query.results);
+    }
+  };
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Klomang - Movie</h1>
+        <input
+          placeholder="Cari Film Favorite"
+          className="Movie-search"
+          onChange={({ target }) => search(target.value)}
+        />
+        <div className="Movie-container">
+          <PopularMovieList />
+        </div>
       </header>
     </div>
   );
-}
+};
 
 export default App;
